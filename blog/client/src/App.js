be870741307d45c74c771b1b65c612c35fe46bd1
user-k-8 <khanyisile842@gmail.com>
2023-11-11 17:06:00 +0200

    App.js

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Blog from "./components/Blog";
 import ViewPost from "./components/ViewPost";
-import TestimonialDetails from "./components/TestimonialDetails";
 import AddPost from "./components/AddPost";
 import Login from "./components/Login";
 import Register from "./components/Register"
@@ -12,9 +11,8 @@ import EditPost from "./components/EditPost";
 function App() {
   return (
         <>
-          <BrowserRouter>
-               <Routes>
-                  
+          <Router>
+               <Routes>                
                   <Route exact path="/blog" element={<Blog/>}/>
                   <Route exact path ="/viewpost" element={<ViewPost/>}/>
                   <Route exact path = "/addpost" element={<AddPost/>} />
@@ -22,7 +20,7 @@ function App() {
                   <Route exact path = "/register" element={<Register/>} />
                    <Route exact path="/editpost" element={<EditPost/>}/>
                </Routes>       
-          </BrowserRouter>
+          </Router>
         </>
   )
 }
